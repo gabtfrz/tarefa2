611f5fe43259ddb1ab6b2b0fbea2c493522df530
joojibananao <125323607+joojibananao@users.noreply.github.com>
2023-05-11 22:51:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+    var nome; //variável do tipo spring
+        nome="Gabriel Ferraz";
+
+    var idade; //variável numérica
+        idade= 18;
+
+    var altura; //variável numérica
+        altura= 1.73;
+
+    var peso; //variável numérica
+        peso= 60;
+
+    var salario; //variável do tipo spring
+        salario="mais ou menos";
+
+    var salario2; //variável numérica
+        salario2=3500;
+
+    var profissao; //variável do tipo spring
+        profissao="tecnico de informatica";
+
+    var quadrado; //variável aritmética
+        quadrado = altura * altura;
+    
+    var imc; //variável aritmética
+        imc = peso / quadrado;
+
+    console.log("Meu nome e ", nome);
+    console.log("Possuo maioridade penal, ou seja, tenho", idade, "anos.");
+    console.log("O meu imc e de ", imc ,". Estou no peso ideal!");
+    console.log("o salario e", salario);
+    
